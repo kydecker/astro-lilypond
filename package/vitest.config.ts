c8c7dcbaa5bf77c1f53ff3616ebd373f4578d25a
Ky Decker <itsevadecker@gmail.com>
2026-07-15 19:25:12 -0400

    Simplify test config

--- package/vitest.config.ts
+++ package/vitest.config.ts
@@ -15,9 +15,6 @@ export default getViteConfig({
 				test: {
 					name: "integration",
 					include: ["tests/integration/**/*.test.ts"],
-					testTimeout: 60_000,
-					hookTimeout: 60_000,
-					maxConcurrency: 10,
 				},
 			},
 		],
